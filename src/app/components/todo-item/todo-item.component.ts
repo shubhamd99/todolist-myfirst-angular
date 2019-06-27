010cad0793b9a71f46895d62bf5ddb28a4551caf
Shubham Dhage <shubhamdhage930@gmail.com>
2019-06-27 22:19:37 +0530

    todolist angular

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+  }
+
+// Set dynamic classes
+  setClasses() {
+    const classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    };
+
+    return classes;
+  }
+
+  onToggle(todo) {
+    // Toggle on UI
+    todo.completed = !todo.completed;
+
+    // Toggle on Server
+    this.todoService.toggleCompleted(todo).subscribe(todoRes =>
+      console.log(todoRes)
+    );
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
+  }
+
+}
